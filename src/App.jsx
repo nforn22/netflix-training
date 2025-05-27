1b8c47bef9f2df31b1d3178aaea14a04f4584f7d
nforn22 <fornarinicolas22@gmail.com>
2025-05-27 14:53:07 +0200

    App.jsx update + globals style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import React from 'react'
+import Section from './components/Section'
+import selectionData from './assets/selectionData.json'
 import './App.css'
 
 function App() {
   return (
-    <>
+    <div className="app">
+      {/* Header Netflix */}
+      <header className="header">
+        <h1 className="logo">NETFLIX</h1>
+      </header>
 
-    </>
-  )
-}
+      {/* Main content */}
+      <main clasname="main">
+        {selectionData.map((section, index) => (
+          <Section 
+            key={index}
+            category={section.category}
+            images={section.images}
+          />
+        ))}
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
